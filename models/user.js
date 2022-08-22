7deedc240ab098110aeb8473ec81711b40dd7892
Zhenhua <zhenhua1109@yahoo.com>
2022-08-22 11:46:47 -0700

    add methods to user class

--- models/user.js
+++ models/user.js
@@ -2,6 +2,12 @@
 
 /** User of the site. */
 
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const BCRYPT_WORK_FACTOR = require("../config.js");
+
+const { UnauthorizedError } = require("../expressError");
+
 class User {
 
   /** Register new user. Returns
@@ -9,11 +15,32 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+
+    const result = await db.query(
+      `INSERT INTO users (username, password, first_name, last_name, phone)
+           VALUES ($1, $2, $3, $4, $5)
+           RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPassword, first_name, last_name, phone]);
+
+    return (result.rows[0]);
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT password
+         FROM users
+         WHERE username = $1`,
+      [username]);
+    const user = result.rows[0];
+
+    if (user) {
+      return (await bcrypt.compare(password, user.password) === true);
+    } else {
+      return false;
+    }
   }
 
   /** Update last_login_at for user */
@@ -25,6 +52,11 @@ class User {
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+    const result = await db.query(
+      `SELECT username, first_name, last_name, phone, join_at, last_login_at
+             FROM users`);
+
+    return result.rows;
   }
 
   /** Get: get user by username
@@ -37,6 +69,13 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
+    const result = await db.query(
+      `SELECT id, first_name, last_name, phone, join_at, last_login_at
+              FROM users
+              WHERE username = $1`,
+              [username]);
+
+    return result.rows[0];
   }
 
   /** Return messages from this user.
@@ -48,7 +87,7 @@ class User {
    */
 
   static async messagesFrom(username) {
-  }
+}
 
   /** Return messages to this user.
    *
@@ -59,7 +98,7 @@ class User {
    */
 
   static async messagesTo(username) {
-  }
+}
 }
 
 
